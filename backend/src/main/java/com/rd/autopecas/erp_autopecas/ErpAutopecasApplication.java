3f657c4c670cf25c07dd6c3f1451e5aa55960eef
Rian <riaann.barbosa@gmail.com>
2026-07-12 02:23:50 -0300

    initial commit

---
+++ backend/src/main/java/com/rd/autopecas/erp_autopecas/ErpAutopecasApplication.java
@@ -0,0 +1,14 @@
+package com.rd.autopecas.erp_autopecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAutopecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAutopecasApplication.class, args);
+        System.out.println("hello");
+	}
+
+}
---
+++ backend/src/test/java/com/rd/autopecas/erp_autopecas/ErpAutopecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rd.autopecas.erp_autopecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpAutopecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
