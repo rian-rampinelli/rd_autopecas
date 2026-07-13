1f94a2aeacdd06f7198636b135aec991e2d63604
Rian <riaann.barbosa@gmail.com>
2026-07-13 17:11:26 -0300

    feat: criando repository de venda e forma de pagamento

---
+++ backend/src/main/java/com/rd/autopecas/erp_autopecas/domain/forma_pagamento/FormaPagamentoRepository.java
@@ -0,0 +1,6 @@
+package com.rd.autopecas.erp_autopecas.domain.forma_pagamento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+}
---
+++ backend/src/main/java/com/rd/autopecas/erp_autopecas/domain/venda/VendaRepository.java
@@ -0,0 +1,6 @@
+package com.rd.autopecas.erp_autopecas.domain.venda;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+}
