a220f7a199af0e7b10a8a2557fe2cc04494ee0e9
Rian <riaann.barbosa@gmail.com>
2026-07-14 06:09:04 -0300

    feat: add oauth2 - adicionando dependençia oauth2 - adicionando value nos securityconfig - adicionando public e privaty key no yaml

--- backend/src/main/java/com/rd/autopecas/erp_autopecas/config/SecurityConfig.java
+++ backend/src/main/java/com/rd/autopecas/erp_autopecas/config/SecurityConfig.java
@@ -1,20 +1,37 @@
 package com.rd.autopecas.erp_autopecas.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /*
+    classe de configuração de segurança,serve como uma camada de verificacao entre
+    a requisicao e o controller
+    configura informacoes como políticas de sessão e autenticação
+     */
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey rsaPublicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey rsaPrivateKey;
+
     @Bean
     private SecurityFilterChain securityFilterChain(HttpSecurity http){
-
         http
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
@@ -23,5 +40,15 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+
     }
 }
